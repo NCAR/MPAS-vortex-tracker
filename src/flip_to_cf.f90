d1ec45ea16108afacad114aa3e0924c5a5c1948f
ahijevyc <ahijevyc@ucar.edu>
2019-05-10 10:35:21 -0600

    needed by mpas_to_latlon

---
+++ src/flip_to_cf.f90
@@ -0,0 +1,22 @@
+    subroutine flip_to_cf(xin,xout,nlon,nlat,nz)
+
+        ! Copied from flip_to_cf_and_box_smooth Feb 16, 2014
+
+        implicit none
+
+        integer, parameter :: RKIND = selected_real_kind(12)
+        integer, intent(in) :: nlon,nlat,nz
+        real (kind=RKIND), intent(in), dimension(nz,nlat,nlon) :: xin
+        real (kind=RKIND), intent(out), dimension(nlon,nlat,nz) :: xout
+ 
+        integer :: i,j,k
+
+        do i=1,nlon
+            do j=1,nlat
+                do k=1,nz
+                    xout(i,j,k) = xin(k,j,i)
+                end do
+            end do
+        end do
+ 
+    end subroutine flip_to_cf
---
+++ src/mpas_filter_cells_by_area.f
@@ -0,0 +1,91 @@
+C NCLFORTSTART
+      subroutine mpas_filter_cells_by_area( nEdgesOnCell,
+     &                              cellsOnCell,areaCell,
+     &                              maxEdges, nVertLevels, nCells,
+     &                              field, radius_km )
+
+      implicit none
+
+      ! input
+
+      integer, INTENT(IN) :: maxEdges,nCells, nVertLevels
+      integer, INTENT(IN) :: nEdgesOnCell(nCells)
+      integer, INTENT(IN) :: cellsOnCell(maxEdges,nCells)
+      double precision, INTENT(IN) :: areaCell(nCells)
+      real*8, INTENT(INOUT) ::  field(nVertLevels,nCells)
+      real, INTENT(IN)      :: radius_km
+C NCLEND
+
+      !  local
+      
+      integer fpass,i,j, k
+      integer fpasses(nCells)
+      real pi
+      parameter (pi=3.141592)
+      integer nnbr   ! neighbor count
+      real*8 nbr_tot ! sum of neighbor values
+      ! tmp = holds new cell values for each smoothing pass
+      real*8  tmp(nVertLevels,nCells)
+
+      ! The number of smoothing passes varies for each cell.
+      ! It is inversely proportional to the radius of each cell
+      ! where the radius of the cell is sqrt(area/pi).
+      ! Smaller cells undergo more smoothing passes while
+      ! larger cells undergo fewer.
+      ! This is not exactly the same as a circular filter with
+      ! uniform or inverse-distance weighting, but it is similar.
+      ! Using neighbors and smoothing passes may be harder to 
+      ! explain but it is quicker than calculating distances from 
+      ! every cell to every cell which a circular filter would
+      ! require.
+      write(0,*)'Entered mpas_filter_by_area'
+
+      write(0,*)'minval(cellsOnCell)=',minval(cellsOnCell)
+      write(0,*)'maxval(cellsOnCell)=',maxval(cellsOnCell)
+      if(maxval(cellsOnCell).gt.nCells
+     c              .or.minval(cellsOnCell).lt.1) then
+        write(0,*)'cellsOnCell(j,i)=',cellsOnCell(j,i)
+        write(0,*)'but must be between 1 and ',nCells
+        write(0,*)'minval(cellsOnCell)=',minval(cellsOnCell)
+        write(0,*)'maxval(cellsOnCell)=',maxval(cellsOnCell)
+        stop
+      endif
+
+      do i=1,nCells
+        fpasses(i) = radius_km/sqrt(1e-6*areaCell(i)/pi)
+      end do
+      write(0,*)'mpas_filter_by_area: calculated fpasses'
+
+      do fpass = 1, maxval(fpasses)
+        ! Each smoothing pass is conservative.
+        do k=1,nVertLevels
+
+           do i=1,nCells
+             tmp(k,i) = field(k,i)
+             ! Smooth cell if fpass <= fpasses(i)
+             ! Average it with neighbors that are also
+             ! under fpass threshold.
+             if(fpass <= fpasses(i))then
+               nnbr = 0
+               nbr_tot = 0.
+               ! Average neighbors
+               do j=1,nEdgesOnCell(i)
+                ! is neighbor <= fpass threshold?
+                if(fpass <= fpasses(cellsOnCell(j,i)))then
+                  nnbr = nnbr + 1 ! increment neighbor count
+                  nbr_tot = nbr_tot + field(k,cellsOnCell(j,i))
+                endif
+               end do
+               ! Smoothed value is average of old value and 
+               ! average of neighbors under fpass threshold.
+               if(nnbr.gt.0) tmp(k,i)=0.5*(tmp(k,i) + nbr_tot/nnbr)
+             endif
+           end do !nCells
+
+           do i=1,nCells
+             field(k,i) = tmp(k,i)
+           end do !nCells
+        end do ! nVertLevels
+      end do ! fpass
+      return
+      end
