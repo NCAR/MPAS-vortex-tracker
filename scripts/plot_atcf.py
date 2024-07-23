c99ff329fed1268d370c1e18e5353ebd7684abae
ahijevyc <ahijevyc@ucar.edu>
2024-07-23 14:45:38 -0600

    Use stormevents.nhc.VortexTrack
    drop warmcore_desc
    
    drop MPAS, kowaleski models
    
    stuff about ten2one

--- scripts/plot_atcf.py
+++ scripts/plot_atcf.py
@@ -5,42 +5,67 @@ Plot tracks meeting length and strength criteria.
 Plots for different basins. 
 """
 import argparse
-import atcf
-import cartopy.feature
-import datetime
-import ibtracs
+import cartopy
+import os
+import sys
+from atcf import (
+        basin_bounds,
+        decorate_ax,
+        iswind_radii_method,
+        origgrid,
+        plot_track,
+        read,
+        write,
+        TClegend,
+        )
 import logging
 import matplotlib.pyplot as plt
-import mpas
-import numpy as np
+import nos
 import pandas as pd
 import pdb
-import re, os
-import subprocess
-import sys
+import re
+from stormevents.nhc import VortexTrack
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s", force=True)
+
+def bestTrack(df:pd.DataFrame) -> pd.DataFrame:
+    # BEST track
+    stormname = df.basin + df.cy + df.valid_time.dt.strftime("%Y")
+    assert stormname.nunique() == 1, 'more than one storm in DataFrame'
+    stormname = stormname.iloc[0]
+    tfile = os.path.join(os.getenv("TMPDIR"), f"{stormname}.csv")
+    if os.path.exists(tfile):
+        logging.warning(f"read {tfile}")
+        idf = pd.read_csv(tfile, parse_dates=["valid_time","track_start_time"], index_col=0)
+    else:
+        logging.warning(f"get {stormname} best track") 
+        idf = VortexTrack(storm=stormname, advisories=["BEST"]).data.rename(
+                columns={
+                    "datetime" : "valid_time",
+                    "latitude" : "lat",
+                    "longitude" : "lon",
+                    "max_sustained_wind_speed" : "vmax",
+                }
+            )
+        logging.warning(f"save {tfile}")
+        idf.to_csv(tfile)
+    # VortexTrack in knots
+    return idf
 
 def main(args):
 
-    debug = args.debug
-    if debug:
-        logger.setLevel(logging.DEBUG)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s", force=True)
 
     logging.debug(args)
 
-    alpha = args.alpha
     ax = args.ax
-    basin       = args.basin
-    clobber      = args.clobber
-    diagdir      = args.diagdir
-    extent       = args.extent
-    initfile     = args.initfile
+    diagdir = args.diagdir
+    extent = args.extent
+    initfile = args.initfile
     initial_time = pd.to_datetime(args.initial_time)
-    min_warmcore_percent = args.min_warmcore_percent
-    models       = args.models
-    origmesh     = args.origmesh
+    models  = args.models
+    origmesh = args.origmesh
     wind_radii_method= args.wind_radii_method
 
     # atcf may be a relative path, so use os.path.realpath() to get absolute path, which has initial date /yyyymmddhh/
@@ -52,9 +77,9 @@ def main(args):
         origmesh = False
     else: 
         origmeshFile = atcf_file + '_origmesh'
-        assert not os.path.isfile(origmeshFile) or clobber, f"{origmeshFile} already exists. Use --clobber to override."
+        assert not os.path.isfile(origmeshFile) or args.clobber, f"{origmeshFile} already exists. Use --clobber to override."
 
-    df = atcf.read(atcf_file)
+    df = read(atcf_file)
 
     # A track has a unique combination of basin, cy, initial_time, and model
     unique_track = ['basin', 'cy', 'initial_time', 'model']
@@ -74,7 +99,7 @@ def main(args):
     if origmesh:
         if not diagdir:
             # Derive diagdir from atcf_file.
-            m = re.search("/[12]\d{9}/", atcf_file)
+            m = re.search(r"/[12]\d{9}/", atcf_file)
             assert m, f"No diagdir supplied, and failed to derive from atcf_file: {atcf_file}"
             # path to diagnostic file original mesh
             diagdir = atcf_file[:m.end()]
@@ -84,7 +109,6 @@ def main(args):
             # path to init.nc file with latCell,lonCell,etc.
             initfile = os.path.join(diagdir,"init.nc")
 
-    warmcore_desc = f"warm core >= {min_warmcore_percent}% of the time"
 
     # If one of unique_tracks columns is constant, put in title. 
     title = ''
@@ -92,13 +116,6 @@ def main(args):
         if df[col].value_counts().count() == 1:
             # Add this column value to title. It doesn't vary.
             title += " " + col + "= " + str(df[col].iloc[0])
-
-    # If warmcore column exists make sure at least one time is warmcore or unknown.
-    if "warmcore" in df.columns:
-        df = df.groupby(unique_track).filter(atcf.iswarmcore, min_warmcore_percent=min_warmcore_percent)
-        logging.info(f'{df.groupby(unique_track).ngroups} after warmcore filter')
-    else:
-        warmcore_desc = f"No warm core filter"
 
     # Remove long tracks (for debugging)
     #df = df.groupby(unique_track).filter(lambda x: x["fhr"].nunique() < 9 )
@@ -110,26 +127,11 @@ def main(args):
         if 'originalmeshfile' in track.columns and track["originalmeshfile"].all():
             logging.warning(f"{track_id} already has original mesh values.")
             return track
-        if model == 'MPAS':
-            if 'origmesh' in atcf_file:
-                logging.warning(f'get_origmesh(): {track.name} already origmesh because "origmesh" is in atcf_file.')
-                # TODO: assert 'origmeshfile' in track.columns or in userdefine1-4 
-                return track
-            logging.info(f'get raw mesh vmax and minp for {len(track.lon)} times, track {track.name}')
-            # Return initfile too if you want to save the lat/lon information as a dictionary and speed things up.
-            # Avoid opening and re-opening the same file over and over again.
-            og, initfile = mpas.origmesh(track, initfile, diagdir, wind_radii_method=wind_radii_method)
         elif (model == 'ECMF' or re.compile("EE[0-9][0-9]$").match(model)):
             assert (track[["rad1","rad2","rad3","rad4"]] == 0).all().all(), (
                     "That is odd. You requested original mesh values but the atcf file already has non-zero radii")
             logging.info(f"{model} getting radius of max wind and radii of 34/50/64 kt wind from {diagdir}")
-            og = atcf.origgrid(track, diagdir, ensemble_prefix=["PF","CF","EE","EE0"], wind_radii_method=wind_radii_method)
-        elif "kowaleski/" in diagdir:
-            # Tack grid nest number to end of original mesh output filename. 
-            # Might help prevent applying d03 grid to d02 tracks, or vice versa.
-            grid="d03"
-            origmeshFile += grid
-            og = atcf.origgridWRF(track, diagdir, grid=grid, wind_radii_method=wind_radii_method)
+            og = origgrid(track, diagdir, ensemble_prefix=["PF","CF","EE","EE0"], wind_radii_method=wind_radii_method)
         else:
             logging.warning(f"no origmesh for {model}")
             return track # used to exit return code=3 but why do that if you have OFCL forecast in same file as model forecasts? that's fine.
@@ -138,49 +140,67 @@ def main(args):
     if origmesh:
         # cannot use .apply on an empty dataframe without losing columns (which you need for "basin" filter below).
         if not df.empty:
-            df = df.groupby(unique_track, as_index=False).apply(get_origmesh, initfile) # as_index=False avoids ValueError: 'basin' is both an index level and a column label, which is ambiguous. 
+            df = df.groupby(unique_track).apply(get_origmesh, initfile)
+        df = df.reset_index(drop=True) # reset_index avoids ValueError: 'basin' is both an index level and a column label, which is ambiguous. 
         # Keep if not a cyclogenesis track or TS strength for at least one time.
         df = df.groupby(unique_track).filter(lambda x: (x["basin"] != "TG").all() or (x["vmax"].max() >=  34))
         logging.info(f"after origmesh {df.groupby(unique_track).ngroups}")
-        atcf.write(origmeshFile, df)
+        write(df, origmeshFile)
         atcf_file = origmeshFile
 
 
     # Regional plots
 
     if ax is None:
+        logging.warning("ax is None, so create fig and ax")
         fig = plt.figure(figsize=(11,7.5))
         ax = plt.axes(projection=cartopy.crs.PlateCarree())
-    ax = atcf.decorate_ax(ax)
+    ax = decorate_ax(ax)
 
     if extent:
         ax.set_extent(extent)
     else:
-        extent = atcf.basin_bounds[basin]
+        extent = basin_bounds[args.basin]
         ax.set_extent(extent)
     
     logging.info(f"plot extent {extent}")
 
-    for (_, cy, initial_time, model), track in df.groupby(unique_track):
+    # Convert ECMWF ensemble Vmax from 10-min to 1-min average
+    ten2one = atcf_file.endswith("scaled_vmax") or atcf_file.endswith("scaled_vmax.Knaff_Zehr_pmin")
+    for (basin, cy, initial_time, model), track in df.groupby(unique_track):
+        linewidth = 0.75
+        linestyle = "solid"
+        alpha = args.alpha
         if model == "OFCL":
-            logging.warning("skip OFCL")
-            continue
+            linewidth = 3
+            linestyle = "dashed"
+            alpha = 1
+            ten2one = False # Assume OFCL Vmaxl is already 1-min average
+        if ten2one:
+            logging.warning(f"convert {model} vmax 10-min average to 1-min") 
+            track["vmax"] = track["vmax"] / nos.one2ten
         start_label = None
         end_label = None
         duration  = track.valid_time.max() - track.valid_time.min()
         logging.info(f'plot_track {cy} {model} {duration}')
-        atcf.plot_track(ax, start_label, track, end_label,
+        plot_track(ax, start_label, track, end_label,
                 label_interval_hours=None,
-                alpha=alpha
+                alpha=alpha,
+                linewidth=linewidth,
+                linestyle=linestyle,
                 )
-    # plot BEST track
-    stormname = atcf.get_stormname(track)
-    if stormname:
-        logging.warning(f"get {stormname} best track") 
-        idf, _ = ibtracs.get_df(stormname=stormname, year = track.valid_time.min().year)
-        atcf.plot_track(ax, stormname, idf, stormname,
+
+    idf = bestTrack(df)
+    plot_track(
+                ax,
+                "",
+                idf, 
+                "",
                 label_interval_hours=24,
-                scale=3)
+                linewidth = 4,
+                linestyle = "solid",
+                scale=2,
+            )
 
 
     # Set title
@@ -201,7 +221,6 @@ def get_parser():
     parser.add_argument('--diagdir', help='path to directory with original model diagnostic files. In case of ensemble, path to directory above member subdirectories i.e. do not include member subdirectory.')
     parser.add_argument('--initfile', help='path to init.nc file with latCell,lonCell,nEdgesOnCell,cellsOnCell')
     parser.add_argument('--initial_time', help='initialization date/hour')
-    parser.add_argument('--min_warmcore_percent', type=float, default=25, help='minimum %% of warm core track times') # double percent sign to print single percent sign
     parser.add_argument('--models', nargs='*', help='only plot this model(s)')
 
     origmesh_group = parser.add_mutually_exclusive_group()
@@ -212,12 +231,13 @@ def get_parser():
     extent_group.add_argument('--basin', help='basin to plot')
     extent_group.add_argument('-e', '--extent', nargs=4, type=float, help='minlon maxlon minlat maxlat (overrides basin)')
 
-    parser.add_argument('--wind_radii_method', type=atcf.iswind_radii_method, default='max', help='method to get wind extent from original mesh')
+    parser.add_argument('--wind_radii_method', type=iswind_radii_method, default='max', help='method to get wind extent from original mesh')
     return parser
 
 def finish(args):
-    legax = atcf.TClegend()
-    fineprint = f"{args} created {datetime.datetime.now()}" # outside basin loop
+    """add legend and fine print"""
+    legax = TClegend()
+    fineprint = f"{args} created {pd.Timestamp.now()}" 
     plt.annotate(text=fineprint, xy=(10,2), xycoords='figure pixels', fontsize=5, wrap=True)
     atcf_file = os.path.realpath(args.atcf_file.name)
     sfx = f".{args.basin}.png" if args.basin else ".png"
@@ -228,6 +248,8 @@ def finish(args):
 
 if __name__ == "__main__":
     # tried to put in main() but run_plot_atcf.py calls main(args) to pass ifile, alpha, extent, ax, etc...
+    logging.warning("get parser")
     parser = get_parser()
     args = parser.parse_args()
+    logging.warning("start main()")
     main(args)
